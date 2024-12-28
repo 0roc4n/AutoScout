bdc58f01cdd3543b722f8151d28855bfb25e0a39
0roc4n <esparragojames@gmail.com>
2024-12-28 19:26:27 +0800

    refractor to AutoDealerShip App

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,19 +3,20 @@ import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ApiService } from '../services/api.service';
-import { IonImg, IonCol, IonRow, IonButton,IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonInput, IonItem, IonThumbnail, IonCard } from '@ionic/angular/standalone';
+import { IonSearchbar, IonImg, IonCol, IonRow, IonButton,IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonInput, IonItem, IonThumbnail, IonCard } from '@ionic/angular/standalone';
 @Component({
   selector: 'app-tab1',
   templateUrl: './tab1.page.html',
   styleUrls: ['./tab1.page.scss'],
   standalone: true,
-  imports: [ IonCard, IonImg, IonThumbnail, IonCol, IonRow, IonButton, IonItem, IonInput, IonIcon, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [ IonSearchbar, IonCard, IonImg, IonThumbnail, IonCol, IonRow, IonButton, IonItem, IonInput, IonIcon, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 
 export class Tab1Page implements OnInit {
 
   items: any[] = [];
   allItems: any[] = [];
+  query!: string;
   private api = inject(ApiService);
 
   constructor() {}
@@ -25,6 +26,26 @@ export class Tab1Page implements OnInit {
   getItems(){
     this.allItems = this.api.items;
     this.items = [...this.allItems]; 
+  }
+
+  async onSearchChange(event: any){
+    console.log(event.detail.value);
+    this.query = event.detail.value.toLowerCase();
+    this.querySearch();
+  }
+
+  querySearch(){
+    this.items = [];
+    if(this.query.length > 0){
+      this.searchItems();
+    }else{
+      this.items = [...this.allItems];
+    }
+  }
+  searchItems(){
+    this.items = this.api.items.filter((item) =>{
+      item.name.toLowerCase().includes(this.query)
+    })
   }
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+// import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
-
+import { IonSearchbar, IonImg, IonCol, IonRow, IonButton,IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonInput, IonItem, IonThumbnail, IonCard } from '@ionic/angular/standalone';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+   imports: [ IonSearchbar, IonCard, IonImg, IonThumbnail, IonCol, IonRow, IonButton, IonItem, IonInput, IonIcon, IonContent, IonHeader, IonTitle, IonToolbar]
 })
 export class Tab2Page {
 
