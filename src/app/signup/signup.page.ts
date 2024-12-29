4f6a6cafe558b637197d4b0a5f87425ddc79241f
0roc4n <esparragojames@gmail.com>
2024-12-29 14:28:11 +0800

    hot fixes

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -3,6 +3,9 @@ import { ToastController } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
+import { initializeApp } from "firebase/app";
+import { environment } from '../../environments/environment';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { IonText, IonLabel,  IonInputPasswordToggle, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonInput, IonItem } from '@ionic/angular/standalone';
 // import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
@@ -14,6 +17,9 @@ import { IonText, IonLabel,  IonInputPasswordToggle, IonButton, IonContent, IonH
   imports: [ RouterModule, IonText, IonLabel, IonInputPasswordToggle, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonInput, IonItem, CommonModule, FormsModule]
 })
 export class SignupPage implements OnInit {
+  oapp = initializeApp(environment.firebase);
+  auth = getAuth(this.oapp);
+
   formData = {
     firstName: '',
     lastName: '',
@@ -21,6 +27,7 @@ export class SignupPage implements OnInit {
     email: '',
     password: ''
   };
+
   async presentToast(message: string, position: 'top' | 'middle' | 'bottom') {
     const toast = await this.toastController.create({
       message: message,
@@ -44,8 +51,23 @@ export class SignupPage implements OnInit {
   onSubmit() {
     // Log form data to the console
     console.log('Form Data:', this.formData);
-    this.presentToast('Please Login to your account', 'middle');
-    this.router.navigate(['/login']);
+    
+    createUserWithEmailAndPassword(this.auth, this.formData.email, this.formData.password)
+      .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        console.log('User signed up successfully:', user);
+        this.presentToast('Please Login to your account', 'middle');
+        this.router.navigate(['/login']);
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error('Error signing up user:', errorMessage);
+        console.error('Error code:', errorCode);
+        // ..
+      });
   }
 }
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase:  {
     apiKey: "AIzaSyBDW9ClwbqaCL1EjB7jmAhlguithZBZwUw",
     authDomain: "autoscout-986c7.firebaseapp.com",
